1cc07b47449de60e21d7fc691f7f20a6b7dbbc33
Jose_Martinez <martinez022jose@gmail.com>
2021-04-02 22:13:49 -0300

    Se modifica funcionalidades js

--- listaDeTareas.js
+++ listaDeTareas.js
@@ -1,89 +1,71 @@
 const refListaDeTareas = document.getElementById('listaDeTareas');
 const title = document.getElementById('title');
 const desc = document.getElementById('desc');
-
-const formTask = document.getElementById('formTask');
+const add = document.getElementById('add');
+const reset = document.getElementById('reset');
 const addTarea = document.getElementById('addTarea');
 const errorDatos = document.getElementById('exit');
 
 let refIndice = null;
-const add = document.getElementById('add');
-let indiceRef = null;
 
-let tareas = [
-	{title: "Hola", descripcion: "sadadsadada"},
-	{title: "chiques", descripcion: "asddadas"}
-];
+let tareas = [];
 
 let updates = [];
 
-const modal = document.getElementById('modales');
-
-
 const getTareas = () => {
-	let tareasRender = tareas.map((tarea,indice)=>`<div class="col-3 m-2">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">${tarea.title}</h5>
-        <p class="card-text">${tarea.descripcion}.</p>
-        <button class="btn btn-danger del" data-indice=${indice}>Eliminar</button>
-		<button class="btn btn-warning edit" data-indice=${indice} data-toggle="modal" data-target="#exampleModal">Editar</button>
-      </div>
-    </div>
-  </div>`
+		let tareasRender = tareas.map((tarea,indice)=>`<div class="col-3 m-2">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">${tarea.title}</h5>
+					<p class="card-text">${tarea.descripcion}.</p>
+					<button class="btn btn-danger del" data-indice=${indice}>Eliminar</button>
+					<button class="btn btn-warning edit" data-indice=${indice} data-toggle="modal" data-target="#exampleModal">Editar</button>
+				</div>
+				</div>
+			</div>`).join('');
 
-	).join('');
+			refListaDeTareas.innerHTML = tareasRender;
 
-	refListaDeTareas.innerHTML = tareasRender;
-	Array.from(document.querySelectorAll('.edit')).forEach((tarea)=>{
-		tarea.addEventListener('click', (e)=>{
-			editarTarea(e);
-		})
-	});
+			Array.from(document.querySelectorAll('.edit')).forEach((tarea)=>{
+				tarea.addEventListener('click', (e)=>{
+					editarTarea(e);
+				})
+			});
 
-	Array.from(document.querySelectorAll('.del')).forEach((tarea)=>{
-		tarea.addEventListener('click', (e)=>{
-			eliminarTarea(e);
-		})
-	});
-	
-
-
-	
+			Array.from(document.querySelectorAll('.del')).forEach((tarea)=>{
+				tarea.addEventListener('click', (e)=>{
+					eliminarTarea(e);
+				})
+			});
 }
 
 const agregarTarea = (e) => {
-	
 	e.preventDefault();
-
-	let accion = addTarea.innerText;
-
-	let datos = {
+	let accionARealizar = addTarea.innerText;
+	let newTarea = {
 		title: title.value,
 		descripcion: desc.value,
 	}
-
+	
 	try{
-		if(datos.title == ''){
+		if(newTarea.title == '' || newTarea.descripcion == ''){
 			throw "Debe completar todos los campos";
 		}else{
-			switch(accion){
+			switch(accionARealizar){
 				case 'Actualizar':
-					tareas[refIndice] = datos;
+					tareas[refIndice] = newTarea;
 					
 				break;
 				default:
-					tareas.push(datos);
+					tareas.push(newTarea);
 				break;
 				
 			}	
-		
-			
 			getTareas();
 			resetForm();
 		}
 	}catch(e){
-		errorDatos.innerHTML+=`<div id="exit" class="alert alert-primary" role="alert">
+		errorDatos.innerHTML+=`<div id="exit" class="alert alert-danger" role="alert">
 		${e}
 	  </div>`;
 	}
@@ -97,32 +79,53 @@ const resetForm = ()=>{
 	},1000);
 }
 
+const resetTareas = ()=>{
+	try{
+		if( tareas.length == 0){
+			throw "No presenta elementos a resetear";
+		}else{
+			tareas = [];
+			getTareas();
+		}
+	}catch(e){
+		errorDatos.innerHTML+=`<div id="exit" class="alert alert-danger" role="alert">
+		${e}
+	  	</div>`;
+	}
+	
+}
+
+const resetError = () => {
+	errorDatos.innerHTML = '';
+}
+
 const eliminarTarea = (e)=>{
 	e.preventDefault();
 	let idRef = e.target.dataset.indice;
 	delete tareas[idRef];
 	getTareas();
-
 }
 
 const editarTarea = (e)=>{
 	e.preventDefault();
-	if(e.target.dataset.indice){
-		tareaUpdate = tareas[e.target.dataset.indice];
+	let indiceTarea = e.target.dataset.indice;
+	if(indiceTarea){
+		tareaUpdate = tareas[indiceTarea];
 		title.value = tareaUpdate.title;
 		desc.value = tareaUpdate.descripcion;
 		addTarea.innerText = "Actualizar";
-		refIndice = e.target.dataset.indice;
+		refIndice = indiceTarea;
 	}else{
 		addTarea.innerText = "Guardar";
 	}
-	
-	
-
 }
 
-addTarea.addEventListener('click', agregarTarea);
-
 getTareas();
+
+addTarea.addEventListener('click', agregarTarea);
+reset.addEventListener('click', resetTareas);
+add.addEventListener('click',resetError);
+
+
 
 
